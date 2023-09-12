308a51118210cadca5e3f11422423464e2bcdcbd
ajengnikita156 <21221753096@smkassalaambandung.sch.id>
2023-09-12 14:56:14 +0700

    baruu

--- src/views/Wishlist.vue
+++ src/views/Wishlist.vue
@@ -21,7 +21,7 @@
               >
                 <img
                   class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                  src="../assets/img/brandd.png"
+                  src="../assets/img/product.jpg"
                   alt=""
                 />
               </a>
